6347610181dbe93073f6596904da90b137ec40b5
tr3dh <timalrot@gmail.com>
2026-04-07 18:42:07 +0200

    ByteSequence als Submoduel ausgelagert

--- src/Alberich/ByteSequence/Batch/clearIconCache.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-taskkill /f /IM explorer.exe
-CD /d %userprofile%\AppData\Local\Microsoft\Windows
-del /f /s /q Explorer\iconcache*.*
-Start explorer.exe
--- src/Alberich/ByteSequence/Batch/installDependenciesAsUser.bat
+++
@@ -1,4 +0,0 @@
-set SCRIPT2=%~dp0installDependencies.bat
-
-echo Starte das Skript als aktueller Benutzer...
-powershell -NoProfile -Command "Start-Process '%SCRIPT2%'"
